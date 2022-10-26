3537772be93c8d789f9cf0f165261c3883b6adcb
GhElizabeth <elizabeth@kpmpower.com>
2022-10-26 10:31:43 -0400

    New version

---
+++ DropdownSapmle.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+
+
+const DropdownSample = () => {
+  return (
+    <nav className='container'>
+          <ul>
+            <li></li>
+            </ul>        
+    </nav>
+  )
+
+} 
+
+export default DropdownSample;
---
+++ README.md
@@ -0,0 +1,2 @@
+# This is the another version of the Inventory Software
+# This version is more user friendly
